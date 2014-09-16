f335a13f9d491941e74513afbaf3107bd66d7afe
Richard Spindler <richard@lateralblast.com.au>
2014-09-16 19:55:27 +1000

    Added mount information

--- methods/mount.rb
+++ methods/mount.rb
@@ -3,7 +3,7 @@
 # Get mount information
 
 def get_mounts()
-  file_name = "/disks/mount.out"
+  file_name = "/disks/mount-v.out"
   file_array = exp_file_to_array(file_name)
   return file_array
 end
@@ -19,15 +19,13 @@ def process_mounts()
     file_array.each do |line|
       line = line.chomp
       line = line.gsub(/ on /," ")
-      if line.match(/^\//)
-        info    = line.split(/\s+/)
-        mount   = info[0]
-        source  = info[1]
-        options = info[2]
-        date    = info[3..-1].join(" ")
-        row     = [ mount, options, ]
-        table   = handle_table("row","",row,table)
-      end
+      info    = line.split(/\s+/)
+      mount   = info[1]
+      source  = info[0]
+      options = info[4]
+      date    = info[5..-1].join(" ")
+      row     = [ mount, options, ]
+      table   = handle_table("row","",row,table)
     end
     table = handle_table("end","","",table)
     puts
@@ -37,15 +35,30 @@ def process_mounts()
     file_array.each do |line|
       line = line.chomp
       line = line.gsub(/ on /," ")
-      if line.match(/^\//)
-        info    = line.split(/\s+/)
-        mount   = info[0]
-        source  = info[1]
-        options = info[2]
-        date    = info[3..-1].join(" ")
-        row     = [ mount, source ]
-        table   = handle_table("row","",row,table)
-      end
+      info    = line.split(/\s+/)
+      mount   = info[1]
+      source  = info[0]
+      options = info[4]
+      date    = info[5..-1].join(" ")
+      row     = [ mount, source ]
+      table   = handle_table("row","",row,table)
+    end
+    table = handle_table("end","","",table)
+    puts
+    title = "Mount Type Information"
+    row   = [ 'Mount', 'Type', 'Date', ]
+    table = handle_table("title",title,row,"")
+    file_array.each do |line|
+      line = line.chomp
+      line = line.gsub(/ on /," ")
+      info    = line.split(/\s+/)
+      mount   = info[1]
+      source  = info[0]
+      type    = info[3]
+      options = info[4]
+      date    = info[5..-1].join(" ")
+      row     = [ mount, type, date ]
+      table   = handle_table("row","",row,table)
     end
     table = handle_table("end","","",table)
   end
