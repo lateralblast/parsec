7fa52626c59341cec5542dcf9c826c0d6be56a7a
Richard Spindler <richard@lateralblast.com.au>
2016-01-19 10:49:20 +1100

    Added processing for zone XML config files

--- methods/zones.rb
+++ methods/zones.rb
@@ -25,6 +25,66 @@ def process_running_zones()
     puts
     puts "No running zone information available"
   end
+  return
+end
+
+# Process zone config
+
+def process_zone_configs()
+  file_name  = "/etc/zones/index"
+  file_array = exp_file_to_array(file_name)
+  if file_array.to_s.match(/[A-Z]|[a-z]|[0-9]/)
+    file_array.each do |line|
+      if !line.match(/^#/) and line.match(/^[a-z]|^[0-9]/)
+        hostname   = line.split(/:/)[0]
+        zone_file  = "/etc/zones/"+hostname+".xml"
+        zone_array = exp_file_to_array(zone_file)
+        if zone_array.to_s.match(/[A-Z]|[a-z]|[0-9]/)
+          title  = "Zone Configuration: "+hostname
+          row    = [ 'Parameter', 'Value' ]
+          table  = handle_table("title",title,row,"")
+          length = zone_array.length
+          zone_array.each_with_index do |xml_line,index|
+            if !xml_line.match(/xml version|lofi/)
+              xml_line = xml_line.gsub(/\<|\/\>|\<\//,"")
+              xml_line = xml_line.gsub(/^\s+|\>/,"")
+              if xml_line.count("=") == 1
+                items = xml_line.split(/=/)
+                param = items[0]
+                value = items[1].gsub(/"/,"")
+                if param.match(/rctl name/)
+                  table = handle_table("line","","",table)
+                end
+                row   = [ param, value ]
+                table = handle_table("row","",row,table)
+              else
+                if xml_line.count("=") > 1
+                  pairs = xml_line.split(/" /)
+                  pairs.each do |pair|
+                    if !pair.match(/zone name/)
+                      items = pair.split("=")
+                      param = items[0]
+                      value = items[1].gsub(/"/,"")
+                      if param.match(/filesystem|attr name|network address/)
+                        table = handle_table("line","","",table)
+                      end
+                      row   = [ param, value ]
+                      table = handle_table("row","",row,table)
+                    end
+                  end
+                end
+              end
+            end
+          end
+          table = handle_table("end","","",table)
+        end
+      end
+    end
+  else
+    puts
+    puts "No configured zone information available"
+  end
+  return
 end
 
 # Process the configured zone information.
@@ -51,10 +111,12 @@ def process_configured_zones()
     puts
     puts "No configured zone information available"
   end
+  return
 end
 
 def process_zones()
   process_running_zones()
   process_configured_zones()
+  process_zone_configs()
   return
 end
