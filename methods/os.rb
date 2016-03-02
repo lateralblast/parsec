0f0462a408cb2cfcc1075e5901f7ecd30ec812b4
Richard Spindler <richard@lateralblast.com.au>
2016-03-03 09:30:28 +1100

    Fixed bug with getting OS update

--- methods/os.rb
+++ methods/os.rb
@@ -44,7 +44,7 @@ end
 
 def get_os_build()
   os_date = get_os_date()
-  if os_date.match(/^11/)
+  if os_date.match(/^11/) and !os_date.match(/\//)
     os_build = os_date.split(".")[1]
   else
     os_build = search_release(3)
