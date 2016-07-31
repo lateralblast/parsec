94f77adbce4be13ae23ddb818eb4a3dfb55b9811
Richard Spindler <richard@lateralblast.com.au>
2016-07-31 10:40:48 +1000

    Updated install scripts

--- utils/sunos/setup_pkgs.sh
+++ utils/sunos/setup_pkgs.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/bash
 
-pkg install ruby
-pkg install gcc
-pkg install imagemagick
-pkg install pigz
-pkg install wget
-pkg install pkg-config
-pkg install git
+id=`id -u`
+
+if [ $id == 0 ]; then
+	echo "Running as root"
+	echo "Installing required packages"
+	pkg install ruby
+	pkg install gcc
+	pkg install imagemagick
+	pkg install pigz
+	pkg install wget
+	pkg install pkg-config
+	pkg install git
+else
+	echo "Not running as root"
+	echo "Require sudo password to install packages"
+	sudo -Sv
+	sudo sh -c "pkg install ruby"
+	sudo sh -c "pkg install gcc"
+	sudo sh -c "pkg install imagemagick"
+	sudo sh -c "pkg install pigz"
+	sudo sh -c "pkg install wget"
+	sudo sh -c "pkg install pkg-config"
+	sudo sh -c "pkg install git"
+fi	
