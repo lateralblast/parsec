fbed07b45e06a775b97f64d919946e2c609fc7f4
Richard Spindler <richard@lateralblast.com.au>
2016-12-27 12:37:28 +1100

    Updated Amazon Linux install scripts

--- utils/amazon-linux/setup_req_gems.sh
+++ utils/amazon-linux/setup_req_gems.sh
@@ -5,37 +5,37 @@ id=`id -u`
 if [ $id == 0 ]; then
 	echo "Running as root"
 	echo "Installing required gems"
-	gem install getopt
-	gem install iconv
-	gem install fileutils
-	gem install hex_string
-	gem install terminal-table
-	gem install unpack
-	gem install enumerate
-	gem install prawn
-	gem install prawn-table
-	gem install fastimage
-	gem install nokogiri -- --use-system-libraries
-	gem install rmagick
-	gem install sinatra
-	gem install bcrypt
+	gem install getopt --no-document
+	gem install iconv --no-document
+	gem install fileutils --no-document
+	gem install hex_string --no-document
+	gem install terminal-table --no-document
+	gem install unpack --no-document
+	gem install enumerate --no-document
+	gem install prawn --no-document
+	gem install prawn-table --no-document
+	gem install fastimage --no-document
+	gem install nokogiri --no-document -- --use-system-libraries
+	gem install rmagick --no-document
+	gem install sinatra --no-document
+	gem install bcrypt --no-document
 else
 	echo "Not running as root"
 	echo "Using sudo password to install gems"
 	sudo -Sv
-	sudo sh -c "gem install getopt"
-	sudo sh -c "gem install iconv"
-	sudo sh -c "gem install fileutils"
-	sudo sh -c "gem install hex_string"
-	sudo sh -c "gem install terminal-table"
-	sudo sh -c "gem install unpack"
-	sudo sh -c "gem install enumerate"
-	sudo sh -c "gem install prawn"
-	sudo sh -c "gem install prawn-table"
-	sudo sh -c "gem install fastimage"
-	sudo sh -c "gem install nokogiri -- --use-system-libraries"
-	sudo sh -c "gem install rmagick"
-	sudo sh -c "gem install sinatra"
-	sudo sh -c "gem install bcrypt"
+	sudo sh -c "gem install getopt --no-document"
+	sudo sh -c "gem install iconv --no-document"
+	sudo sh -c "gem install fileutils --no-document"
+	sudo sh -c "gem install hex_string --no-document"
+	sudo sh -c "gem install terminal-table --no-document"
+	sudo sh -c "gem install unpack --no-document"
+	sudo sh -c "gem install enumerate --no-document"
+	sudo sh -c "gem install prawn --no-document"
+	sudo sh -c "gem install prawn-table --no-document"
+	sudo sh -c "gem install fastimage --no-document"
+	sudo sh -c "gem install nokogiri --no-document -- --use-system-libraries"
+	sudo sh -c "gem install rmagick --no-document"
+	sudo sh -c "gem install sinatra --no-document"
+	sudo sh -c "gem install bcrypt --no-document"
 fi
 
--- utils/amazon-linux/setup_req_rpms.sh
+++ utils/amazon-linux/setup_req_rpms.sh
@@ -9,6 +9,7 @@ if [ $id == 0 ]; then
 	yum -y install gcc
 	yum -y install ruby-devel
 	yum -y install ImageMagick
+	yum -y install ImageMagick-devel
 	yum -y install pigz
 	yum -y install star
 else
@@ -19,6 +20,7 @@ else
 	sudo sh -c "yum -y install gcc"
 	sudo sh -c "yum -y install ruby-devel"
 	sudo sh -c "yum -y install ImageMagick"
+	sudo sh -c "yum -y install ImageMagick-devel"
 	sudo sh -c "yum -y install pigz"
 	sudo sh -c "yum -y install star"
 fi
